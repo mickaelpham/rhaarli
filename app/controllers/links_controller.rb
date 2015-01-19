b1b73a6fa8c17ebf7c28600f03cc59007bf215ca
Mickael Pham <mickael.pham@gmail.com>
2015-01-19 12:01:16 -0800

    Added methods to links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,22 +1,60 @@
 class LinksController < ApplicationController
+  before_action :set_link, only: [:show, :edit, :update, :destroy]
+
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
+    @links = Link.all
   end
 
   def show
   end
 
+  def new
+    @link = Link.new
+  end
+
+  def edit
+  end
+
+  def create
+    @link = Link.new(link_params)
+
+    respond_to do |format|
+      if @link.save
+        format.html { redirect_to @link, notice: 'Link was successfully created.' }
+        format.json { render :show, status: :created, location: @link }
+      else
+        format.html { render :new }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+    end
+  end
+
   def update
+    respond_to do |format|
+      if @link.update(link_params)
+        format.html { redirect_to @link, notice: 'Link was successfully updated.' }
+        format.json { render :show, status: :ok, location: @link }
+      else
+        format.html { render :edit }
+        format.json { render json: @link.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @link.destroy
+    respond_to do |format|
+      format.html { redirect_to link_url, notice: 'Link was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_link
+    @link = Link.find(params[:id])
+  end
+
+  def link_params
+    params.require(:link).permit(:url, :title, :description, :private)
   end
 end
