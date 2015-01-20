215d221f4f667803c3da18f84b558548d1e4801f
Mickael Pham <mickael.pham@gmail.com>
2015-01-19 18:19:44 -0800

    Added validation rules to link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,18 @@
 class Link < ActiveRecord::Base
+  # One link can have multiple tags
   has_and_belongs_to_many :tags
 
+  # Sort by `updated_at` DESC when retrieving all links
   default_scope { order(updated_at: :desc) }
+
+  # URL is the only required field
+  validates :url, presence: true
+
+  # Default values after initialization
+  after_initialize :init
+
+  def init
+    # TODO put `private` default value in settings
+    self.private = false if self.private.nil?
+  end
 end
